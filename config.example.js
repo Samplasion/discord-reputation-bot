89044decf8673bc031ec050d456d29ff77df3258
Samplasion <ercolino2004@gmail.com>
2018-12-06 17:48:47 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -4,6 +4,7 @@ module.exports.config = {
   owner: '178261738364338177', //bot owner's ID. You can get this with discord developer mode
   logChannel: '508203322298204160',
   lftChannel: '465087084433113088',
+  lftVC: 'vcIdHere',
   enableCooldowns: false
 
 }

30a68b76b7c19021e1323790025857e08df89617
Samplasion <ercolino2004@gmail.com>
2018-12-06 17:42:17 +0100

    Added the "Move to VC" part
    You'll need a config entry `lftVC`, which is the ID of the VC you want the lft users to be moved in

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,6 +1,12 @@
-module.exports = async (client, reaction, user, message) => {
-
-  if(message.channel.id !== client.config.lftChannel) return
-
-  //if(reaction.)
+module.exports = async (client, reaction, user) => {
+  let message = reaction.message;
+  if (message.channel.id !== client.config.lftChannel) return;
+  if (message.author.id == user.id) return reaction.remove();
+  if (reaction.emoji.name !== "✅") return;
+  
+  let messager = message.member,
+      reacter = await message.guild.fetchMember(user.id);
+  
+  messager.setVoiceChannel(client.config.lftVC)
+  reacter.setVoiceChannel(client.config.lftVC)
 }
